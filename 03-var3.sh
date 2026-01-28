2abb08d6f3f9c45d1d48608456ee1942efd42b9a
sivakumar <sivakumarthalla123@gmail.com>
2026-01-28 20:41:25 +0530

    variables

---
+++ 03-var3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+number=$2
+
+if [ $number>20 ]; then
+echo "the given number $number : is greater than 20"
+elif [ $number -eq 20]; then
+echo "the given number $number : is equal than 20"
+
+else
+echo "the given number $number : is less than 20"
+
+fi
