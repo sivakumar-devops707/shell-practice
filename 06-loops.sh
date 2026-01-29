a0e34b101a8172bc62646770329b1569020070cf
sivakumar <sivakumarthalla123@gmail.com>
2026-01-29 20:06:13 +0530

    variables

--- 06-loops.sh
+++ 06-loops.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 user=$(id -u)
+log_folder="/var/log/shell-scripts"
+log_file="/var/log/shell-scripts/$0.log"
 
 if [ $user -ne 0 ]; then
-    echo "please run as sudo user / root user"
+    echo "please run as sudo user / root user" | tee -a $log_file
    exit 1
 
 fi
 
 validate(){
     if [ $1 -ne 0 ]; then
-      echo "installing $2 failure...."
+      echo "installing $2 failure...." | tee -a $log_file
       exit 1
     else
 
-      echo "installing  $2  success"
+      echo "installing  $2  success" | tee -a $log_file
     fi
 }
 
 for package in $@ 
 do 
-    dnf list installed $package
+    dnf list installed $package &>>$log_file
     if [ $? -ne 0 ]; then
         echo "$package not installed...installing  $package now ...."
-        dnf install $package -y
+        dnf install $package -y &>>$log_file
         validate $? $package
     else
       
