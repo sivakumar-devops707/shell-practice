b864f5e4591d618c52d2350c3fd6e36b057a916e
sivakumar <sivakumarthalla123@gmail.com>
2026-01-29 19:04:59 +0530

    variables

---
+++ 06-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+user=$(id -u)
+
+if [ $user -ne 0 ]; then
+    echo "please run as sudo user / root user"
+   exit 1
+
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+echo "installing $2 failure...."
+exit 1
+else
+
+echo "installing  $2  success"
+fi
+}
+
+for package in $@ 
+do 
+   dnf list installed nginx
+     if [ $? -ne 0 ]; then
+      echo "$package not installed...installing  $package now ...."
+      dnf install $package -y
+      validate $? $package
+    else
+
+      echo "installing  $package  skipped"
+    fi
+done
